cb56f2ae6624b18a14099c1e8e35d166fd2df4f2
mattaaronmagnaye <123015615+mattaaronmagnaye@users.noreply.github.com>
2023-07-25 19:42:41 -0700

    added initial description docs

--- mystring.cpp
+++ mystring.cpp
@@ -1,22 +1,35 @@
+/*
+* Program: mystring.cpp (Assignment 11P: Classes with DM 1)
+* Author: Matt Aaron Magnaye
+* Instructor: Professor David Harden
+* Date: 07/25/2023
+* Description: Writing string class called MyString.
+*
+*/
 #include "mystring.h"
 #include <cstring>
 
 namespace cs_mystring {
 
+    // Constructor: Initialize MyString with a c-string.
     MyString::MyString(const char* s) {
         cstring = new char[strlen(s) + 1];
         strcpy(cstring, s);
     }
 
+    // Copy constructor: Initialize MyString with another MyString.
     MyString::MyString(const MyString& source) {
         cstring = new char[strlen(source.cstring) + 1];
         strcpy(cstring, source.cstring);
     }
 
+    // Destructor: Deallocates the memory that was previously reserved for this string.
     MyString::~MyString() {
         delete[] cstring;
     }
 
+    // Overloaded assignment operator to copy the content from one 
+    // MyString to another.
     MyString& MyString::operator=(const MyString& right) {
         if (this != &right) {
             delete[] cstring;
@@ -26,14 +39,17 @@ namespace cs_mystring {
         return *this;
     }
 
+    // Overloaded [] operator to get the character at a specific index.
     char MyString::operator[](int index) const {
         return cstring[index];
     }
 
+    // Overloaded [] operator to get/set the character at a specific index.
     char& MyString::operator[](int index) {
         return cstring[index];
     }
 
+    // Overloaded operators for comparing two MyString objects.
     bool operator<(const MyString& left, const MyString& right) {
         return strcmp(left.cstring, right.cstring) < 0;
     }
@@ -58,11 +74,13 @@ namespace cs_mystring {
         return strcmp(left.cstring, right.cstring) != 0;
     }
 
+    // Overloaded stream insertion operator to output the content of the MyString.
     std::ostream& operator<<(std::ostream& out, const MyString& source) {
         out << source.cstring;
         return out;
     }
 
+    // Returns the length of the c-string in this MyString.
     int MyString::length() const {
         return strlen(cstring);
     }
--- mystring.h
+++ mystring.h
@@ -1,18 +1,42 @@
+/*
+* Program: mystring.h (Assignment 11P: Classes with DM 1)
+* Author: Matt Aaron Magnaye
+* Instructor: Professor David Harden
+* Date: 07/25/2023
+* Description: Writing string class called MyStringg.
+*
+*/
 #include <iostream>
 
 namespace cs_mystring {
+
+    // MyString class for representing and manipulating sequences of characters.
     class MyString {
         private:
-            char* cstring;
+            char* cstring;  // private variable to store the c-string.
+
         public:
+            // Constructor: Initialize MyString with a c-string.
+            // If no argument is provided, it defaults to an empty string.
             MyString(const char* s = "");
+
+            // Copy constructor: Initialize MyString with another MyString.
             MyString(const MyString& source);
+
+            // Destructor: Deallocates the memory that was previously reserved 
+            // for this string.
             ~MyString();
+
+            // Overloaded assignment operator to copy the content from one MyString to another.
             MyString& operator=(const MyString& right);
 
+            // Overloaded [] operator to get the character at a specific index.
             char operator[](int index) const;
+
+            // Overloaded [] operator to get/set the character at a specific index.
             char& operator[](int index);
 
+            // Overloaded operators for comparing two MyString objects.
             friend bool operator<(const MyString& left, const MyString& right);
             friend bool operator<=(const MyString& left, const MyString& right);
             friend bool operator>(const MyString& left, const MyString& right);
@@ -20,7 +44,10 @@ namespace cs_mystring {
             friend bool operator==(const MyString& left, const MyString& right);
             friend bool operator!=(const MyString& left, const MyString& right);
 
+            // Overloaded stream insertion operator to output the content of the MyString.
             friend std::ostream& operator<<(std::ostream& out, const MyString& source);
+
+            // Returns the length of the c-string in this MyString.
             int length() const;
     };
 }
